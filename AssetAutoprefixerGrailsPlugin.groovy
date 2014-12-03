5232fbfa4291f6818c04d54d39a133db828d3a7a
peh <peh@kunstsysteme.com>
2014-12-03 13:20:20 +0100

    added scm and issue tracker information

--- AssetAutoprefixerGrailsPlugin.groovy
+++ AssetAutoprefixerGrailsPlugin.groovy
@@ -1,31 +1,15 @@
 class AssetAutoprefixerGrailsPlugin {
-    def version = "1.0"
-    def grailsVersion = "2.2 > *"
+    def version = '1.0'
+    def grailsVersion = '2.2 > *'
     def dependsOn = ['asset-pipeline': '2.0 > *']
-    def title = "Asset Autoprefixer Plugin"
-    def author = "Philipp Eschenbach"
-    def authorEmail = "philipp@uberall.com"
-    def description = '''
-Asset-Pipeline extension to call autoprefixer on less and css files
-'''
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/asset-autoprefixer"
-
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
-    def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
-    def organization = [ name: "uberall", url: "https://uberall.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-    // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
-
+    def title = 'Asset Autoprefixer Plugin'
+    def author = 'Philipp Eschenbach'
+    def authorEmail = 'philipp@uberall.com'
+    def description = 'Asset-Pipeline extension to call autoprefixer on less and css files'
+    def documentation = 'http://grails.org/plugin/asset-autoprefixer'
+    def license = 'APACHE'
+    def organization = [name: 'uberall', url: 'https://uberall.com/']
+    def developers = [[name: 'David Carter', email: 'david@uberall.com']]
+    def issueManagement = [system: 'GitHub', url: 'https://github.com/uberall/grails-asset-autoprefixer/issues']
+    def scm = [url: 'https://github.com/uberall/grails-asset-autoprefixer']
 }
