6f553aeb8295d22d2f21390dc8d0250011f37f25
peh <peh@kunstsysteme.com>
2015-02-06 16:34:35 +0100

    switched to binary gradle plugin

--- AssetAutoprefixerGrailsPlugin.groovy
+++ AssetAutoprefixerGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class AssetAutoprefixerGrailsPlugin {
-    def version = '1.0'
+    def version = '1.1.3'
     def grailsVersion = '2.2 > *'
     def title = 'Asset Autoprefixer Plugin'
     def author = 'Philipp Eschenbach'
@@ -8,7 +8,7 @@ class AssetAutoprefixerGrailsPlugin {
     def documentation = 'http://grails.org/plugin/asset-autoprefixer'
     def license = 'APACHE'
     def organization = [name: 'uberall', url: 'https://uberall.com/']
-    def developers = [[name: 'David Carter', email: 'david@uberall.com']]
+    def developers = [[name: 'Philipp Eschenbach', email: 'philipp@uberall.com'], [name: 'David Carter', email: 'david.carter@uberall.com']]
     def issueManagement = [system: 'GitHub', url: 'https://github.com/uberall/grails-asset-autoprefixer/issues']
     def scm = [url: 'https://github.com/uberall/grails-asset-autoprefixer']
 }
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,22 +1,29 @@
-grails.project.work.dir = 'target'
-
-grails.project.dependency.resolver = 'maven'
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
-
-    inherits 'global'
-    log 'warn'
-
+    inherits("global") {
+    }
+    log "warn"
     repositories {
         grailsCentral()
         mavenLocal()
         mavenCentral()
+        mavenRepo "https://raw.github.com/uberall/uberall-plugins/mvn-repo/"
+    }
+    dependencies {
+        compile('com.uberall.plugins:autoprefixer-asset-pipeline:1.0.2')
     }
 
     plugins {
-        build ':release:3.0.1', ':rest-client-builder:2.0.3', {
+        build(":release:3.0.1", ":rest-client-builder:1.0.3") {
             export = false
         }
 
-        compile(':asset-pipeline:2.0.9')
+        compile(":asset-pipeline:2.0.13") {
+            export = false
+        }
     }
 }
+
