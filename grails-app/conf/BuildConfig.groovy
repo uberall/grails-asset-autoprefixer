46761b9a8d0c17c9977f95ce848ff7b29244fb29
peh <peh@kunstsysteme.com>
2015-02-10 10:53:26 +0100

    added our own repo definitions

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,6 +2,11 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.dependency.resolver = "maven"
+
+grails.project.repos.uberallPlugins.url = "file://${System.getProperty('uberall.plugins.home')}"
+grails.project.repos.uberallPlugins.type = "maven"
+grails.project.repos.uberallPlugins.portal = null
+
 grails.project.dependency.resolution = {
     inherits("global") {
     }
